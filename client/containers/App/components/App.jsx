40a04bea746ae016cafae582e1b0782d88bc9e37
Sergey Okishev <so@aspirity.com>
2018-05-23 18:45:10 +0700

    better code

--- client/containers/App/components/App.jsx
+++ client/containers/App/components/App.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Router } from 'react-router';
+import { hot } from 'react-hot-loader';
+import createHistory from 'history/createBrowserHistory';
 import Routes from './Routes';
+
+const history = createHistory();
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Routes />
+        <Router history={history}>
+          <Routes />
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
--- client/index.js
+++ client/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { Router } from 'react-router';
-import createHistory from 'history/createBrowserHistory';
 import App from './containers/App';
 
-const history = createHistory();
-
-const render = (Component) => {
+const render = () => {
   ReactDOM.render(
-    <AppContainer>
-      <Router history={history}>
-        <Component />
-      </Router>
-    </AppContainer>,
+    <App />,
     document.getElementById('app')
   );
 };
 
-render(App);
-
-if (module.hot) {
-  module.hot.accept('./containers/App', () => {
-    const NextRootContainer = require('./containers/App').default;
-    render(NextRootContainer);
-  });
-}
+render();
