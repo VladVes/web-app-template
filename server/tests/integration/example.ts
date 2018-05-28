43130d22eb3c991f4e4b048d21655ef9c4ceb373
Sergey Okishev <so@aspirity.com>
2018-05-28 14:38:41 +0700

    Express and mongodb tests added.

--- server/tests/integration/example.ts
+++ server/tests/integration/example.ts
@@ -1,12 +1,11 @@
-import { expect } from 'chai';
 import 'mocha';
+import { expect } from 'chai';
 // process.env.NODE_ENV = 'test';
 
-describe('/GET version', () => {
+describe('simple test', () => {
   it('it should return 0.0.1', (done) => {
     const one = '0.0.1';
     expect(one).to.equal('0.0.1');
     done();
   })
 });
-
