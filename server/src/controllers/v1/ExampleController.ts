55cf49a9ae597d4e9eaf14caa891b61d07575b21
Григорий <gmanzhur@aspirity.com>
2018-06-14 16:42:33 +0700

    Fetch bitcoin price from our server

--- server/src/controllers/v1/ExampleController.ts
+++ server/src/controllers/v1/ExampleController.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import * as VError from 'verror';
+import axios from 'axios';
 import { ExampleService } from '../../services';
 import Logger from '../../Logger';
-import passport from '../../middlewares/Passport';
 import BaseController from '../BaseController';
+import config from '../../config';
 
 const logger = new Logger();
 
@@ -10,6 +12,7 @@ class ExampleController extends BaseController {
   public init(): void {
     this.router.get('/', this.get);
     this.router.get('/sum', this.getSum);
+    this.router.get('/bitcoin', this.getBitcoinPrice);
   }
 
   public get(req: Request, res: Response, next: NextFunction): Response {
@@ -26,6 +29,18 @@ class ExampleController extends BaseController {
 
     const sum = ExampleService.add(first, second);
     return res.json({ sum });
+  }
+
+  public async getBitcoinPrice(req: Request, res: Response, next: NextFunction): Promise<Response|void> {
+    const bitcoinUrl = config.get('bitcoinUrl');
+
+    try {
+      const response = await axios.get(bitcoinUrl);
+
+      return res.json({ price: response.data[0].price_usd });
+    } catch (err) {
+      return next(err instanceof Error ? err : new VError(err));
+    }
   }
 }
 
