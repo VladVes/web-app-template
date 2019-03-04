0a1c2a42824a88d2cbdd48fb05912b07b795f363
Sergey Okishev <so@aspirity.com>
2019-03-04 11:37:46 +0700

    Alerts added. ColorPicker from HFS added. delete/put methods added for axios support. Image component updated, PropType provided.

--- client/src/containers/App/components/App.jsx
+++ client/src/containers/App/components/App.jsx
@@ -4,12 +4,19 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import { hot } from 'react-hot-loader';
+import Alert from 'react-s-alert';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
 import MainModal from 'Shared/modal/MainModal';
 import configureStore from '../Redux/configureStore';
 import Routes from './Routes';
 import Main from './Main';
+import CustomAlertTemplate from './CustomAlertTemplate';
+import { config as i18nextConfig } from '../../../translations/index';
 
 import '../styled/GlobalStyles';
+
+i18next.init(i18nextConfig);
 
 const history = createBrowserHistory();
 const store = configureStore(history);
@@ -18,11 +25,20 @@ const App = ({ children }) => (
   <div className="App">
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Main>
-          <Routes />
-          <MainModal />
-          {children}
-        </Main>
+        <I18nextProvider i18n={i18next}>
+          <Main>
+            <Routes />
+            <MainModal />
+            <Alert
+              effect="stackslide"
+              stack={{ limit: 10 }}
+              offset={88}
+              timeout={15 * 1000}
+              contentTemplate={CustomAlertTemplate}
+            />
+            {children}
+          </Main>
+        </I18nextProvider>
       </ConnectedRouter>
     </Provider>
   </div>
--- client/src/shared/Image.jsx
+++ client/src/shared/Image.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import FileProp from 'PropTypes/FileProp';
 
 export default class Image extends Component {
   static propTypes = {
-    image: PropTypes.shape({ _id: PropTypes.string }).isRequired,
+    image: FileProp,
+    defaultImagePath: PropTypes.string,
   };
 
-  getFileUrl = id => `/uploads/${id}`;
+  static defaultProps = {
+    image: null,
+    defaultImagePath: null,
+  };
+
+  getFileUrl = (image, defaultImage) => (image ? `/uploads/${image._id}` : defaultImage);
 
   render() {
-    const { image, ...other } = this.props;
-    if (!image || !image._id) return null;
-    const url = this.getFileUrl(image._id);
+    const { image, defaultImagePath, ...other } = this.props;
+    if (!defaultImagePath && !(image && image._id)) return null;
+
+    const url = this.getFileUrl(image, defaultImagePath);
     return (<img alt={url} src={url} {...other} />);
   }
 }
